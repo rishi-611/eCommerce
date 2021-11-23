ed591783bd8780c7e9a681fccd4d828315db341f
rishi dubey <rishi18454@gmail.com>
2021-11-23 16:23:08 +0530

    added footer and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App">App</div>;
-}
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <main>
+        <h1> App</h1>
+      </main>
+      <Footer />
+    </React.Fragment>
+  );
+};
 
 export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container fluid>
+        <Row>
+          <Col className="text-center py-3 bg-dark text-light">
+            &#169; ECommerce
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
