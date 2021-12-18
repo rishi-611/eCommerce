0479ba9c66ea0c74aeab047adb84b99d383ddb8a
rishi dubey <rishi18454@gmail.com>
2021-12-18 22:17:37 +0530

    added static form to profile screen

--- client/src/scenes/Profile.js
+++ client/src/scenes/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { Container, Image } from "react-bootstrap";
+import { Image, Form, Button } from "react-bootstrap";
 
 const Profile = () => {
   const { user, isLoggedIn } = useSelector((state) => state.user);
@@ -10,8 +10,8 @@ const Profile = () => {
   if (!isLoggedIn) return <Navigate to="/login" />;
 
   return (
-    <Container className="bg-primary p-4">
-      <div className="row">
+    <React.Fragment>
+      <div className="row bg-primary p-4">
         <div className="col-12 col-sm-4 d-flex justify-content-center align-items-center">
           <Image
             roundedCircle
@@ -20,12 +20,46 @@ const Profile = () => {
             fluid
           ></Image>
         </div>
-        <div className="col-12 col-sm-8 d-flex flex-column justify-content-center">
+        <div className="col-12 col-sm-8 pt-3 pt-sm-0 d-flex flex-column justify-content-center align-items-center align-items-sm-start ">
           <h3 className="profile-name text-light">{user.name}</h3>
           <h5 className="profile-email text-light">{user.email}</h5>
         </div>
       </div>
-    </Container>
+
+      <Form className="mt-5">
+        <Form.Group className="mb-5" controlId="edit-email">
+          <Form.Label>Edit Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your Name"
+            autoComplete="off"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="edit-pass-curr">
+          <Form.Label>Current Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Your current Password"
+            autoComplete="on"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>New Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter New Password"
+            autoComplete="off"
+          />
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            autoComplete="off"
+          />
+        </Form.Group>
+      </Form>
+    </React.Fragment>
   );
 };
 
