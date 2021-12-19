4bb71fe0c4a25626d10d345f1f20fe4dfaa7d4c0
rishi dubey <rishi18454@gmail.com>
2021-12-19 17:50:28 +0530

    comverted profile static form to controlled form, added submit buttons with proper layout

--- client/src/scenes/Profile.js
+++ client/src/scenes/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { Image, Form, Button } from "react-bootstrap";
@@ -6,6 +6,30 @@ import { Image, Form, Button } from "react-bootstrap";
 const Profile = () => {
   const { user, isLoggedIn } = useSelector((state) => state.user);
   const dispatch = useDispatch();
+
+  const [name, setName] = useState(user?.name || "");
+
+  const [passForm, setPassForm] = useState({
+    currPass: "",
+    newPass: "",
+    newPass2: "",
+  });
+
+  const handleNameSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+  };
+
+  const handlePassForm = (e) => {
+    e.preventDefault();
+    const { currPass, newPass, newPass2 } = passForm;
+
+    if (newPass != newPass2) {
+      return console.log("Passwords must match");
+    }
+
+    console.log(passForm);
+  };
 
   if (!isLoggedIn) return <Navigate to="/login" />;
 
@@ -26,39 +50,84 @@ const Profile = () => {
         </div>
       </div>
 
-      <Form className="mt-5">
-        <Form.Group className="mb-5" controlId="edit-email">
-          <Form.Label>Edit Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your Name"
-            autoComplete="off"
-          />
-        </Form.Group>
+      <div className="mt-5 ">
+        <Form
+          className="profile-form-box  text-center"
+          onSubmit={handleNameSubmit}
+        >
+          {" "}
+          <Form.Group className="mb-5" controlId="edit-email">
+            <Form.Label>Edit Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your Name"
+              autoComplete="off"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              name="name"
+              required
+            />
+            <Button type="submit">
+              Edit<i className="fas fa-pencil-alt"></i>
+            </Button>
+          </Form.Group>
+        </Form>
 
-        <Form.Group className="mb-3" controlId="edit-pass-curr">
-          <Form.Label>Current Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Your current Password"
-            autoComplete="on"
-          />
-        </Form.Group>
+        <Form onSubmit={handlePassForm}>
+          <Form.Group className="mb-3" controlId="edit-pass-curr">
+            <Form.Label>Current Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Your current Password"
+              autoComplete="on"
+              onChange={(e) =>
+                setPassForm((passForm) => ({
+                  ...passForm,
+                  [e.target.name]: e.target.value,
+                }))
+              }
+              value={passForm.currPass}
+              name="currPass"
+              required
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>New Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter New Password"
-            autoComplete="off"
-          />
-          <Form.Control
-            type="password"
-            placeholder="Confirm Password"
-            autoComplete="off"
-          />
-        </Form.Group>
-      </Form>
+          <Form.Group className="mb-3">
+            <Form.Label>New Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter New Password"
+              autoComplete="off"
+              onChange={(e) =>
+                setPassForm((passForm) => ({
+                  ...passForm,
+                  [e.target.name]: e.target.value,
+                }))
+              }
+              value={passForm.newPass}
+              name="newPass"
+              required
+            />
+            <Form.Control
+              type="password"
+              placeholder="Confirm New Password"
+              autoComplete="off"
+              onChange={(e) =>
+                setPassForm((passForm) => ({
+                  ...passForm,
+                  [e.target.name]: e.target.value,
+                }))
+              }
+              value={passForm.newPass2}
+              name="newPass2"
+              required
+            />
+            <Button type="submit" className="d-block m-auto">
+              Edit<i className="fas fa-pencil-alt"></i>
+            </Button>
+          </Form.Group>
+        </Form>
+      </div>
     </React.Fragment>
   );
 };
